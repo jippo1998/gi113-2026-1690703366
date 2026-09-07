ab35f1e1d107783931ef448d8896edf908c02e82
jippo1998 <pannam903@gmail.com>
2026-09-07 16:00:36 +0700

    add lab04
    Create lab04

---
+++ lab04/lab04/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+* Student ID :1690703366
+* Name       :เมธปรียา บุญมาวงศ์
+* Section    :129D
+* No.        :N/A
+* Course     : GI113 Computer Programming (GI)
+*/
+namespace lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+----------------------------------------------------+"); 
+            Console.WriteLine("|                    NEW ADVENTURE                   |");
+            Console.WriteLine("+----------------------------------------------------+"); 
+            Console.WriteLine("Name your hero: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine($"\nWelcome, {playerName}! Your journey begins . . .\"");
+
+
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|      CHARACTER CREATION      |");
+            Console.WriteLine("+------------------------------+");
+            Console.Write("Name your character: ");
+            string charName = Console.ReadLine();
+            Console.Write("Choose a class (1-3): ");
+            bool classOk = int.TryParse(Console.ReadLine(), out int classNum);
+            Console.Write("Starting luck (0.0-10.0): ");
+            bool luckOk = double.TryParse(Console.ReadLine(), out double luck);
+            Console.WriteLine($"{charName} the Class-{classNum} adventurer enters the dungeon. Luck: {luck}");
+
+
+        }
+   
+    }
+} 
