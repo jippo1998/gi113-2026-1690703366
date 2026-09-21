b9a79f241a743fc5b8bc9b4de7a7938109a78917
jippo1998 <pannam903@gmail.com>
2026-09-21 15:19:31 +0700

    add lab06

---
+++ lab06/lab06/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lives = 0;//ตัวแปรหลักเพื่อเช็ก
+
+            if (lives == 0) //ในวงเล็บคือเงื่อนไขที่ต้องเป็นจริง
+
+            {
+                //บล็อกของโค๊ดที่ทำงาน เมื่อเงื่อนไขเป้นจริง
+                Console.WriteLine("Game Over");
+            }
+            else
+            {
+                //บล็อกของโค๊ดที่ทำงาน เมื่อเงื่อนไขเป้นเท็จ
+                Console.WriteLine("Continue to play");
+            }
+
+            //เมื่อเงื่อนไขทำงานเสร็จแล้วเงื่อนไข เงื่อนไขไม่ตรงเลย โค๊ดทำงานต่อ
+            Console.WriteLine("continue to run");
+        }
+    }
+}
